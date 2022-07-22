474be94476241221462dac80f069bbb78c341d4e
Julius Jacobsohn <julius.jacobsohn@hotmail.com>
2022-07-22 13:09:24 +0200

    Add model files

---
+++ StudyPlanVisualizer/Models/Module.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyPlanVisualizer.Models
+{
+    public class Module
+    {
+        public string Name { get; set; } = "Unknown";
+        public double? Grade { get; set; }
+        public string? HexColor { get; set; }
+    }
+}
---
+++ StudyPlanVisualizer/Models/Semester.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyPlanVisualizer.Models
+{
+    public class Semester
+    {
+        public string Name { get; set; }
+        public List<Module> Modules { get; set; }
+    }
+}
