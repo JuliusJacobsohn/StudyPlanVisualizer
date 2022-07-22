636c4364b2185e647dbce9cfafc4d16ae809a3a0
Julius Jacobsohn <julius.jacobsohn@hotmail.com>
2022-07-22 13:09:33 +0200

    Add code to read in text file

--- StudyPlanVisualizer/Program.cs
+++ StudyPlanVisualizer/Program.cs
@@ -1,16 +1,58 @@
-﻿using System.Drawing;
+﻿using StudyPlanVisualizer.Models;
+using System.Drawing;
 using System.Drawing.Imaging;
+using System.Globalization;
 
 const int SEMESTER_WIDTH = 300;
+const int MODULE_HEIGHT = 50;
 
-using (Bitmap newImage = new(300, 300))
+var data = File.ReadAllLines("data.csv");
+List<Semester> semesters = ReadInput(data);
+
+int targetWidth = semesters.Count * SEMESTER_WIDTH;
+int targetHeight = semesters.Max(m => m.Modules.Count) * MODULE_HEIGHT;
+
+using (Bitmap newImage = new(targetWidth, targetHeight))
 {
-
     // Crop and resize the image.
-    Rectangle destination = new Rectangle(0, 0, 200, 120);
-    using (Graphics graphic = Graphics.FromImage(newImage))
+    using (Graphics graphics = Graphics.FromImage(newImage))
     {
-        graphic.DrawRectangle(new Pen(Color.Red), destination);
+        var groups = data.GroupBy(d => d.Split(";")[0]);
+        foreach (var group in groups)
+        {
+            DrawGroup(group, graphics);
+        }
     }
-    newImage.Save("test.jpg", ImageFormat.Jpeg);
+    newImage.Save("data.jpg", ImageFormat.Jpeg);
+}
+
+static void DrawGroup(IGrouping<string, string> group, Graphics graphics)
+{
+    Rectangle destination = new Rectangle(0, 0, 200, 120);
+    graphics.DrawRectangle(new Pen(Color.Red), destination);
+}
+
+static List<Semester> ReadInput(string[] data)
+{
+    List<Semester> semesters = new List<Semester>();
+    foreach (var line in data)
+    {
+        var parts = line.Split(";");
+        var existingSemester = semesters.FirstOrDefault(m => m.Name == parts[0]);
+        if (existingSemester == null)
+        {
+            existingSemester = new Semester { Name = parts[0], Modules = new List<Module>() };
+            semesters.Add(existingSemester);
+        }
+
+        Module newModule = new Module
+        {
+            Name = parts[1],
+            HexColor = parts[2],
+            Grade = double.Parse(parts[3], CultureInfo.InvariantCulture),
+        };
+        existingSemester.Modules.Add(newModule);
+    }
+
+    return semesters;
 }
