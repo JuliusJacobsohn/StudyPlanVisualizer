0e3f82e02e4fd0507e1db4dde93af5e35ed42c81
Julius Jacobsohn <julius.jacobsohn@hotmail.com>
2022-07-22 12:52:06 +0200

    Add project files.

---
+++ StudyPlanVisualizer/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+
+const int SEMESTER_WIDTH = 300;
+
+using (Bitmap newImage = new(300, 300))
+{
+
+    // Crop and resize the image.
+    Rectangle destination = new Rectangle(0, 0, 200, 120);
+    using (Graphics graphic = Graphics.FromImage(newImage))
+    {
+        graphic.DrawRectangle(new Pen(Color.Red), destination);
+    }
+    newImage.Save("test.jpg", ImageFormat.Jpeg);
+}
